d1c305d5d47b45eb49cb0fd548583e3b96618cb4
estellebotton1 <estelle.botton@doordash.com>
2026-05-26 22:08:09 -0400

    nav bar

--- pages/homepage-final.jsx
+++ pages/homepage-final.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import NavBar from "../src/NavBar";
 
 var c = {
   red: "#A33B2E",
--- pages/the-shop.jsx
+++ pages/the-shop.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import NavBar from "../src/NavBar";
 
 const C = {
   cream: "#FBF6F0",
