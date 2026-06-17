e2312671e764c23f4de96f30863628abd0c5f8a6
estelle botton <ebotton@princeton.edu>
2026-06-17 19:20:10 -0400

    ssnity

--- tallest-tiptoes/schemaTypes/guide.js
+++ tallest-tiptoes/schemaTypes/guide.js
@@ -5,7 +5,7 @@ export default {
   fields: [
     { name: 'title', title: 'Title', type: 'string' },
     { name: 'sortOrder', title: 'Display Order (1 = first)', type: 'number' },
-    { name: 'category', title: 'Category', type: 'string', options: { list: ['Restaurants', 'Hosting', 'Weekend', 'Travel'] } },
+    { name: 'category', title: 'Category', type: 'string', options: { list: ["What I'm Eating", "Where I'm Going", "How I'm Hosting", "Things I'm Liking"] } },
     { name: 'date', title: 'Date', type: 'date' },
     { name: 'image', title: 'Cover Image', type: 'image' },
     { name: 'images', title: 'Additional Images', type: 'array', of: [{ type: 'image' }] },
