01ffb6a846dfd16a3cbf54eb5d8a7bf81b5f9233
estelle botton <ebotton@princeton.edu>
2026-06-18 19:57:29 -0400

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import TermsAndConditions from "../pages/terms-and-conditions";
 import PrivacyPolicy from "../pages/privacy-policy";
 import Homepage from "../pages/homepage-final";
@@ -8,18 +9,27 @@ import TheShop from "../pages/the-shop";
 import HisNotHers from "../pages/his-not-hers";
 import TheGuide from "../pages/the-guide";
 
+function ScrollToTop() {
+  var location = useLocation();
+  useEffect(function () {
+    window.scrollTo(0, 0);
+  }, [location.pathname, location.state]);
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
-        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/" element={<Homepage />} />
         <Route path="/the-forum" element={<TheForum />} />
         <Route path="/the-edit" element={<TheEdit />} />
         <Route path="/the-shop" element={<TheShop />} />
         <Route path="/his-not-hers" element={<HisNotHers />} />
         <Route path="/the-guide" element={<TheGuide />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
     </BrowserRouter>
   );
